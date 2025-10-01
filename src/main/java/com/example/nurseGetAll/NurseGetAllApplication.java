c5d49eb125b1dffb572d51419408c04a8e5c199b
MarvinEsteban <tommyesgon0202@gmail.com>
2025-10-01 15:46:23 +0200

    First commit

---
+++ src/main/java/com/example/nurseGetAll/NurseGetAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurseGetAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseGetAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseGetAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nurseGetAll/NurseGetAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurseGetAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseGetAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
